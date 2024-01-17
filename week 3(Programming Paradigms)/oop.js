f14e4f1e915e5c0a931cc1e858dcaef95017e863
harshkumar1302 <bs824536@gmail.com>
2024-01-17 22:05:18 +0530

    Learned OOP and this. ( it is used to refer to the object itself without specifying the object's name.)

---
+++ week 3(Programming Paradigms)/oop.js
@@ -0,0 +1,51 @@
+// Object-oriented Programming Paradigms
+
+// write some code that calculate the total cost of buying a pair of shoes.
+
+// according to functional programming 
+// Input or variable
+var priceOfProduct = 5678;
+var taxForTheProduct = 1.2;
+// function for this code
+function purchase1(priceOfProduct, taxForTheProduct) {
+    return priceOfProduct * taxForTheProduct;
+}
+// output
+var totalPrice = purchase1(priceOfProduct, taxForTheProduct);
+console.log(`Total Price is INR ${totalPrice}.`)
+
+// Now according to Object-oriented programming we do:
+
+var purchase2 = {
+    shoes: 100,
+    stateTax: 1.2,
+    totalPrice: function () {
+        var calculation = purchase2.shoes + (purchase2.shoes * purchase2.stateTax);
+        console.log(`Total Price: ${calculation} $`);
+    }
+}
+purchase2.totalPrice();
+console.log(purchase2.shoes);
+console.log(purchase2.stateTax);
+
+
+
+var purchase3 = {
+    shirt: 59,
+    stateTax: 1.2,
+    totalPrice: function () {
+        var calculation = this.shirt + (this.shirt * this.stateTax);
+        console.log(`Total Price: ${calculation} $`);
+    }
+}
+purchase3.totalPrice();
+
+var purchase4 = {
+    shirt: 49,
+    stateTax: 1.2,
+    totalPrice: function () {
+        var calculation = this.shirt + (this.shirt * this.stateTax);
+        console.log(`Total Price: ${calculation} $`);
+    }
+}
+purchase4.totalPrice();
