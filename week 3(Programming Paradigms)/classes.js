fc15bfb6652c934f0c32c9d2fd2b44d4bc249e65
harshkumar1302 <bs824536@gmail.com>
2024-01-18 19:25:13 +0530

    Constructors, Inheritance, Creating Classes

--- week 3(Programming Paradigms)/classes.js
+++ week 3(Programming Paradigms)/classes.js
@@ -64,3 +64,35 @@ var toEat = new Noon();
 var toDrink = new Drink();
 toEat.todayLunch();
 toDrink.todayLunch();
+
+
+// Example 3
+class Train {
+    constructor(color, lightsOn) {
+        this.color = color;
+        this.lightsOn = lightsOn;
+    }
+    toggleLights() {
+        this.lightsOn = !this.lightsOn;
+    }
+    lightsStatus() {
+        console.log('Lights on?', this.lightsOn);
+    }
+    getSelf() {
+        console.log(this);
+    }
+    getPrototype() {
+        var proto = Object.getPrototypeOf(this);
+        console.log(proto);
+    }
+}
+
+var myFirstTrain = new Train('red', false);
+console.log(myFirstTrain);
+var mySecondTrain = new Train('blue', false);
+var myThirdTrain = new Train('blue', false);
+var train4 = new Train('red', false);
+train4.toggleLights();
+train4.lightsStatus();
+train4.getSelf();
+train4.getPrototype();
