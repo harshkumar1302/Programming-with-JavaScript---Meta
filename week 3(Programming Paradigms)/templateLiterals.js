d2a579d5066e39a12c665f8d732b39ba3e552b81
harshkumar1302 <bs824536@gmail.com>
2024-01-25 00:21:13 +0530

    Task related to destructuring and template litreals

---
+++ week 3(Programming Paradigms)/templateLiterals.js
@@ -0,0 +1,25 @@
+// // ES5 strings
+
+// let noMultiLine = "No multi-line strings in ES5";
+// console.log("Did you know? " + noMultiLine);
+
+
+
+
+// // ES6 Template Literals
+
+// console.log(`Using ES6
+// backricks,
+// multi-line
+// strings are
+// possible`)
+
+// let greet = "Hello";
+// let place = "World";
+// console.log(`${greet} - ${place} !`);
+
+// // it's possible to perform arithmetic operation inside a template literal expression
+// console.log(`${1 + 1 + 1 + 1 + 1} stars!`)
+
+
+
